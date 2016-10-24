173fa88a2acb930b60336551fdd156efaecfc175
Enrique P <ername@users.noreply.github.com>
2016-10-24 00:11:04 -0500

    testing 2

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,56 @@
 class GameController < ApplicationController
-  def user_plays_rock
+def lets_play
+  render("game/main_page.html.erb")
+end
+
+def user_plays_rock
     render("game/play_rock.html.erb")
-  end
+
+end
+
+def user_plays_scissors
+    render("game/play_scissors.html.erb")
+end
+
+def user_plays_paper
+    render("game/play_paper.html.erb")
+end
+
+@user_move = "rock"
+@computer_move = ["rock","paper","scissors"].sample
+if @user_move==@computer_move
+  "You tied"
+elsif @computer_move=="paper"
+  "You lose"
+elsif @computer_move=="scissors"
+        "You win"
+end
+
 end
 #if it has the @ it becomes a variable that last for the entire document
 # if we create a variable within the def and end it ends with the end and therefore does not apply to other pages
 # any logic that is not required for the output should be on the rb folder
 # for images type img src="folder where it is" and width
+# user_move = gets.chomp
+
+# puts "You chose #{user_move}."
+
+# computer_move = ["rock", "paper", "scissors"].sample
+
+# puts "The computer chose #{computer_move}."
+
+# if user_move == computer_move
+  # puts "You tied!"
+# elsif user_move == "paper" && computer_move == "rock"
+  # puts "You win!"
+# elsif user_move == "paper" && computer_move == "scissors"
+  # puts "You lose!"
+# elsif user_move == "scissors" && computer_move == "rock"
+  # puts "You lose!"
+# elsif user_move == "scissors" && computer_move == "paper"
+  # puts "You win!"
+# elsif user_move == "rock" && computer_move == "paper"
+  # puts "You lose!"
+# elsif user_move == "rock" && computer_move == "scissors"
+#   puts "You win!"
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+get("", {:controller => "game", :action => "lets_play"})
   get("/rock", {:controller => "game", :action => "user_plays_rock"})
+get("/scissors", {:controller => "game", :action => "user_plays_scissors"})
+get("/paper", {:controller => "game", :action => "user_plays_paper"})
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
